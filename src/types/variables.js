6c0a1811a0147da4799fc3918f5463a43518bd3e
Francisco Jarma <francisco.jarma@onetree.com>
2022-06-01 12:20:56 -0300

    Variables

---
+++ src/types/variables.js
@@ -0,0 +1,17 @@
+//GLOBAL VARIABLES
+export const ID_ROL_ABONADO = 'B35AFF2D-A14B-4C78-B9E1-EB5437D21FC4';
+export const ID_ROL_TECNICO = '3EF5B486-2604-44E6-BA2C-D9F78BF7A612';
+export const ID_ROL_SECRETARIO = '8EFB5688-4C88-4F50-86D1-EFF2929F6C92';
+export const ID_ROL_ADMIN = 'B35AFF2D-A14B-4C78-B9E1-EB5437D21FC41';
+export const ID_ROL_JEFE = '26EB30A8-71D9-4B1F-B40E-2119F55F8BA2';
+
+export const ID_CAMBIO_DOMICILIO_CABLE = 14;
+export const ID_CAMBIO_DOMICILIO_INTERNET = 15;
+export const ID_MEDIO_PAGO_FACILIDAD = 10;
+export const ID_MEDIO_PAGO_EFECTIVO= 1;
+//MENSAJES
+export const ERROR_AUTENTICACION = "Error: Request failed with status code 401";
+export const ABONADO_CREADO_CORRECTAMENTE = "El abonado ha sido registrado correctamente";
+export const PAGO_CREADO_CORRECTAMENTE = "El pago ha sido registrado correctamente";
+export const CAMBIO_DOMICILIO_CORRECTO = "El cambio de domicilio ha sido registrado correctamente.";
+export const CAMBIO_SERVICIO_CORRECTO = "El cambio de servicio ha sido registrado correctamente.";
